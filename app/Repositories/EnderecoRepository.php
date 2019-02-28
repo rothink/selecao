8daded54e58bc07f3624f01a549a85771944c780
rothink <rodrigoluz@live.com>
2019-02-28 02:15:47 -0300

    repositories

---
+++ app/Repositories/EnderecoRepository.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Endereco;
+use Prettus\Repository\Eloquent\BaseRepository;
+
+/**
+ * @package namespace App\Repositories;
+ */
+class EnderecoRepository extends BaseRepository
+{
+    /**
+     * @return string
+     */
+    public function model()
+    {
+        return Endereco::class;
+    }
+
+    /**
+     * @param $arrParams
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function save($arrParams)
+    {
+        if(empty($arrParams['pessoa_id'])) {
+            return response()->json(['msg' => 'Por favor, informe o ID da pessoa'], 404);
+        }
+
+        if(empty($arrParams['cep'])) {
+            return response()->json(['msg' => 'Por favor, informe o cep'], 404);
+        }
+
+        if(empty($arrParams['logradouro'])) {
+            return response()->json(['msg' => 'Por favor, informe o logradouro'], 404);
+        }
+
+        if(empty($arrParams['bairro'])) {
+            return response()->json(['msg' => 'Por favor, informe o bairro'], 404);
+        }
+
+        if(empty($arrParams['localidade'])) {
+            return response()->json(['msg' => 'Por favor, informe a localidade'], 404);
+        }
+
+        try {
+            $endereco = Endereco::create($arrParams);
+            return response()->json(['data' => $endereco], 200);
+        } catch (\Exception $e) {
+            return response()->json(['msg' => 'Erro ao cadastrar Endereco', 'detail' => $e->getMessage()], 500);
+        }
+    }
+
+    /**
+     * @param $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function show($id)
+    {
+        if(empty($id)) {
+            return response()->json(['msg' => 'Por favor, informa o ID do endereco'], 400);
+        }
+        $endereco = \App\Endereco::find($id);
+
+        if(!$endereco) {
+            return response()->json(['msg' => 'Nenhum endereco encontrado'], 404);
+        }
+        return response()->json(['data' => $endereco], 200);
+    }
+
+    /**
+     * @param $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function destroy($id)
+    {
+        if(empty($id)) {
+            return response()->json(['msg' => 'Por favor, informa o ID do endereco'], 400);
+        }
+
+        $endereco = Endereco::find($id);
+
+        if(!$endereco) {
+            return response()->json(['msg' => 'Nenhum endereco encontrado'], 404);
+        }
+
+        try {
+            Endereco::destroy($id);
+            return response()->json(['msg' => 'Excluido com sucesso'], 200);
+        } catch (\Exception $e) {
+            return response()->json(['msg' => 'Erro ao excluir Endereco', 'detail' => $e->getMessage()], 500);
+        }
+    }
+}
---
+++ app/Repositories/PessoaRepository.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Endereco;
+use App\Pessoa;
+use Prettus\Repository\Eloquent\BaseRepository;
+use App\Repositories\EnderecoRepository;
+
+/**
+ * @package namespace App\Repositories;
+ */
+class PessoaRepository extends BaseRepository
+{
+    /**
+     * @var EnderecoRepository
+     */
+    protected $enderecoRepository;
+
+    public function __construct(EnderecoRepository $enderecoRepository){
+        $this->enderecoRepository = $enderecoRepository;
+    }
+    /**
+     * Specify Model class name
+     * @return string
+     */
+    public function model()
+    {
+        return Pessoa::class;
+    }
+
+    /**
+     * @param $arrParams
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function save($arrParams)
+    {
+        if(empty($arrParams['nome'])) {
+            return response()->json(['msg' => 'Por favor, informe o nome'], 404);
+        }
+
+        if(empty($arrParams['sobrenome'])) {
+            return response()->json(['msg' => 'Por favor, informe o sobrenome'], 404);
+        }
+
+        try {
+            $pessoa = Pessoa::create($arrParams);
+            if(isset($arrParams['enderecos']) && !empty($arrParams['enderecos'])) {
+                foreach($arrParams['enderecos'] as $endereco){
+                    $endereco['pessoa_id'] = $pessoa->id;
+                    $this->enderecoRepository->save($endereco);
+                }
+            }
+            return response()->json(['data' => $pessoa, 'msg' => 'Cadastro realizado com com sucesso'], 200);
+        } catch (\Exception $e) {
+            return response()->json(['msg' => 'Erro ao cadastrar', 'detail' => $e->getMessage()], 500);
+        }
+    }
+
+    /**
+     * @param $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function show($id)
+    {
+        if(empty($id)) {
+            return response()->json(['msg' => 'Por favor, informa o ID da pessoa'], 400);
+        }
+
+        $pessoa = \App\Pessoa::find($id);
+
+        if(!$pessoa) {
+            return response()->json(['msg' => 'Nenhuma pessoa encontrada'], 404);
+        }
+
+        return response()->json(['data' => $pessoa], 200);
+    }
+
+    /**
+     * @param $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function destroy($id)
+    {
+        if(empty($id)) {
+            return response()->json(['msg' => 'Por favor, informa o ID da pessoa'], 400);
+        }
+
+        $pessoa = Pessoa::find($id);
+
+        if(!$pessoa) {
+            return response()->json(['msg' => 'Nenhuma pessoa encontrado'], 404);
+        }
+
+        try {
+            $enderecos = $this->buscarEnderecosPorIdPessoa($id);
+            if($enderecos){
+                foreach ($enderecos as $endereco) {
+                    Endereco::destroy($endereco->id);
+                }
+            }
+            Pessoa::destroy($id);
+            return response()->json(['msg' => 'Excluido com sucesso'], 200);
+        } catch (\Exception $e) {
+            return response()->json(['msg' => 'Erro ao excluir Usuario e seus enderecos', 'detail' => $e->getMessage()], 500);
+        }
+    }
+
+    /**
+     * @param $idPessoa
+     * @return mixed
+     * Busca todos os enderecos pelo id da pessoa.
+     */
+    public function buscarEnderecosPorIdPessoa($idPessoa)
+    {
+        return \App\Endereco::where('pessoa_id', '=', $idPessoa)->get();
+    }
+
+    /**
+     * @param $idPessoa
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function enderecoByIdPessoa($idPessoa)
+    {
+        if(empty($idPessoa)) {
+            return response()->json(['msg' => 'Por favor, informa o ID da pessoa'], 400);
+        }
+        $endereco = $this->buscarEnderecosPorIdPessoa($idPessoa);
+
+        if(count($endereco) == 0) {
+            return response()->json(['msg' => 'Nenhum endereço encontrado'], 404);
+        }
+
+        return response()->json(['data' => $endereco]);
+    }
+}
